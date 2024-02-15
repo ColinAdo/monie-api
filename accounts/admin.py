d3ccb961b9a68dff1b9aae354fa66dfd6ecceab6
ColinAdo <colinadodev30@gmail.com>
2024-02-15 22:13:48 +0300

    Add Custom user to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "username",
+        "email",
+        "phone_number",
+        "is_active",
+        "is_superuser",
+    ]
+
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("phone_number",)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("phone_number",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
