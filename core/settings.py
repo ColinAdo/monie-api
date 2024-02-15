d095ceee365093b85de122ed01e6b5506131548c
ColinAdo <colinadodev30@gmail.com>
2024-02-15 21:32:30 +0300

    Installed phonenumber_field

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party libraries
+    'phonenumber_field',
+
     # local
     'accounts.apps.AccountsConfig',
 ]
