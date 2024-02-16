fcd150beab1324e07c571d6f5d341ff3492d219d
ColinAdo <colinadodev30@gmail.com>
2024-02-16 22:00:06 +0300

    Add Transaction model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from accounts.models import Account
+from accounts.models import Account, Transaction
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ["user", "balance", "created_date"]
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["account", "amount", "transaction_type", "created_date"]
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from accounts.choices import TRANSACTION_TYPES
+
 class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="accounts", on_delete=models.CASCADE)
     balance = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
@@ -8,3 +10,13 @@ class Account(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
+    transaction_type = models.CharField(max_length=200, choices=TRANSACTION_TYPES)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    def __str__(self):
+        return self.transaction_type
