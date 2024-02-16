c54edab2e7eda80767df38c742c0a5af5140797a
ColinAdo <colinadodev30@gmail.com>
2024-02-16 22:16:01 +0300

    Add Test to Account model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import Account
+
+class AccountTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username="TestUser",
+            email="test@example.com",
+        )
+
+        cls.account = Account.objects.create(
+            user=cls.user,
+            balance=1000
+        )
+
+    def test_account_content(self):
+        account = Account.objects.get(id=1)
+
+        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(account.user, self.user)
+        self.assertEqual(account.balance, 1000)
+
+    def test_return_string(self):
+        self.assertEqual(str(self.account), self.user.username)
