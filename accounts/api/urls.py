964033955f515187d39a6945e5651f43c16fe70f
ColinAdo <colinadodev30@gmail.com>
2024-02-20 08:26:19 +0300

    Add transaction type based endpoints

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -5,9 +5,17 @@ from accounts.api.views import (
     AccountDetailView,
     TransactionApilistView,
     TrnsactionDetailView,
+    AirtimeTransaction,
+    SentTransaction,
+    ReceivedTransaction,
+    WithdrawTransaction,
 )
 
 urlpatterns = [
+    path('transactions/withdraw/', WithdrawTransaction.as_view(), name="withdraw"),
+    path('transactions/received/', ReceivedTransaction.as_view(), name="received"),
+    path('transactions/sent/', SentTransaction.as_view(), name="sent"),
+    path('transactions/airtime/', AirtimeTransaction.as_view(), name="airtime"),
     path('transactions/', TransactionApilistView.as_view(), name="transactions"),
     path('transactions/<int:pk>/', TrnsactionDetailView.as_view(), name="transaction-detail"),
     path('accounts/', AccountApiView.as_view(), name="accounts"),
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -23,3 +23,23 @@ class TransactionApilistView(generics.ListCreateAPIView):
 class TrnsactionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+class AirtimeTransaction(generics.ListAPIView):
+    queryset = Transaction.objects.filter(transaction_type='airtime')
+    serializer_class = TransactionSerializer
+
+
+class SentTransaction(generics.ListAPIView):
+    queryset = Transaction.objects.filter(transaction_type='sent')
+    serializer_class = TransactionSerializer
+
+
+class ReceivedTransaction(generics.ListAPIView):
+    queryset = Transaction.objects.filter(transaction_type='received')
+    serializer_class = TransactionSerializer
+
+
+class WithdrawTransaction(generics.ListAPIView):
+    queryset = Transaction.objects.filter(transaction_type='withdraw')
+    serializer_class = TransactionSerializer
+
