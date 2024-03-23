2e0ddaae13d7a8ad4d257bcdc8a536b6181388d0
ColinAdo <colinadodev30@gmail.com>
2024-03-23 09:49:38 +0300

    Add clean code to model

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,6 @@ class Transaction(models.Model):
     content = models.CharField(max_length=200, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True, null=True)
 
-
     def __str__(self):
         return self.transaction_type
 
