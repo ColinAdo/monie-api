02e03eaadecfbdc0e331e54d43eac5c28de29a6f
ColinAdo <colinadodev30@gmail.com>
2024-03-23 09:50:51 +0300

    Add clean code

--- auths/forms.py
+++ auths/forms.py
@@ -2,12 +2,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from auths.models import CustomUser
 
-
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("phone_number",)
-
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
