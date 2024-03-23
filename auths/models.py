6c71ad3d1848a5f35bf5210f7a7c80ab473e7737
ColinAdo <colinadodev30@gmail.com>
2024-03-23 09:51:13 +0300

    Add clean code models

--- auths/models.py
+++ auths/models.py
@@ -2,6 +2,5 @@ from django.contrib.auth.models import AbstractUser
 
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 class CustomUser(AbstractUser):
     phone_number = PhoneNumberField(blank=True)
