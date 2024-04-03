2822dba9eb291c9964e5ba4d65e51b4f2464b8b7
ColinAdo <colinadodev30@gmail.com>
2024-04-03 16:43:11 +0300

    Add test to custom user model

--- auths/tests.py
+++ auths/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username="TestUser",
+            email="test@example.com",
+        )
+
+    def test_user_contents(self):
+        self.assertEqual(self.user.username, 'TestUser')
