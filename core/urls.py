b8fe548bb703a420042ee7aaff2bba9c18db3919
ColinAdo <colinadodev30@gmail.com>
2024-04-27 10:57:47 +0300

    Add djoser urlConf

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.jwt')),
     path('api/v1/', include('accounts.api.urls')),
 ]
