56bd1d9066f4f4512633181e05bf8dbbb7543c34
ColinAdo <colinadodev30@gmail.com>
2024-04-29 17:34:49 +0300

    Add custom nuser create serializer

--- auths/serializers.py
+++ auths/serializers.py
@@ -1,42 +1,50 @@
-from djoser.serializers import UserCreateSerializer
-from django.contrib.auth import get_user_model
-from django.conf import settings
+
 import boto3
+import random
+from django.conf import settings
+from djoser.serializers import UserCreateSerializer
+from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):  # Inherit from UserCreateSerializer.Meta
-        model = get_user_model()
+    class Meta(UserCreateSerializer.Meta):
+        model = CustomUser
         fields = ('username', 'phone_number')
 
-
     def create(self, validated_data):
-        user = get_user_model().objects.create(**validated_data)
+        validated_data['password'] = None
+        pin = settings.SMS_MESSAGE
+        validated_data['pin'] = make_password(str(pin))  # Hash the pin
+        return super().create(validated_data)
+    
+    def validate(self, attrs):
+        if 'password' in attrs:
+            del attrs['password']
+        return attrs
+    
+    # Deluxe pin: 16417
 
-        # Send your sms message.
-        client = boto3.client(
-            "sns",
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION_NAME
-        )
+    # Send your sms message.
+    # client = boto3.client(
+    #     "sns",
+    #     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    #     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    #     region_name=settings.AWS_REGION_NAME
+    # )
 
-        response = client.publish(
-            PhoneNumber=settings.SMS_MOBILE,
-            Message=settings.SMS_MESSAGE,
-            MessageAttributes={
-                'AWS.SNS.SMS.SenderID': {
-                    'DataType': 'String',
-                    'StringValue': settings.SENDER_ID
-                }
-            }
-        )
+    # response = client.publish(
+    #     PhoneNumber=settings.SMS_MOBILE,
+    #     Message=settings.SMS_MESSAGE,
+    #     MessageAttributes={
+    #         'AWS.SNS.SMS.SenderID': {
+    #             'DataType': 'String',
+    #             'StringValue': settings.SENDER_ID
+    #         }
+    #     }
+    # )
+    print(settings.SMS_MESSAGE)
 
-        # Generating a random PIN might be better for security
-        # user.pin = generate_random_pin()
+    
 
-        # Instead, let's just save the message sent for reference
-        user.pin = settings.SMS_MESSAGE
-        user.save()
 
-        return user
