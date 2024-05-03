520ddd9afb8a60c57c2fbb34da2c4270a1faca07
ColinAdo <colinadodev30@gmail.com>
2024-05-03 15:31:53 +0300

    Removed pin field from the model

--- auths/models.py
+++ auths/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.hashers import make_password
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -8,11 +7,6 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=200, unique=False)
     phone_number = PhoneNumberField(unique=True)
     pin = models.CharField(max_length=200)
-
-    def save(self, *args, **kwargs):
-        # hash pin field before saving 
-        self.pin = make_password(str(self.pin))
-        super().save(*args, **kwargs)
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['username']
