0d49881e440afcc8b4efc53efd1c3e2922597a1a
ColinAdo <colinadodev30@gmail.com>
2024-05-03 15:32:34 +0300

    Add custom jwt tokens and authentications

--- auths/views.py
+++ auths/views.py
@@ -1,6 +1,112 @@
-# from djoser.views import UserViewSet
-# from .serializers import CustomUserCreateSerializer
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
-# class CustomUserCreateView(UserViewSet):
-#     serializer_class = CustomUserCreateSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
+
+class SignUpView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        username = request.data.get('username')
+        phone_number = request.data.get('phone_number')
+
+        if not username or not phone_number:
+            return Response({"error": "Username and phone number are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        User = get_user_model()
+        User.objects.create(
+            username=username,
+            phone_number=phone_number,
+            # Hash the password
+            password=make_password(str(settings.SMS_MESSAGE))  
+        )
+
+        return Response({"username": username, "phone_number": phone_number, "password": settings.SMS_MESSAGE}, status=status.HTTP_201_CREATED)
+    
+
+
+class LogoutView(APIView):
+    authentication_classes = []
+    permission_classes = []
+    def post(self, request, *args, **kwargs):
+        response = Response(status=status.HTTP_204_NO_CONTENT)
+        response.delete_cookie('access')
+        response.delete_cookie('refresh')
+
+        return response
+    
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+            refresh_token = response.data.get('refresh')
+
+            response.set_cookie(
+                'access',
+                access_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE
+            )
+            response.set_cookie(
+                'refresh',
+                refresh_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE
+            )
+
+        return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh')
+
+        if refresh_token:
+            request.data['refresh'] = refresh_token
+
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+
+            response.set_cookie(
+                'access',
+                access_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE
+            )
+
+        return response
+
+
+class CustomTokenVerifyView(TokenVerifyView):
+    def post(self, request, *args, **kwargs):
+        access_token = request.COOKIES.get('access')
+
+        if access_token:
+            request.data['token'] = access_token
+
+        return super().post(request, *args, **kwargs)
