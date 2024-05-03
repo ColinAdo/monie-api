26b160ffbec3f94e111d18137229d0bdbdaf2240
ColinAdo <colinadodev30@gmail.com>
2024-05-03 15:33:19 +0300

    Add URLConf to the custom views

--- auths/urls.py
+++ auths/urls.py
@@ -1,9 +1,17 @@
-# from django.urls import path
-# from .views import CustomUserCreateView
+from django.urls import path
+from .views import (
+    SignUpView,
+    LogoutView,
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView
+)
 
 
-# urlpatterns = [
-#     path('users/',
-#          CustomUserCreateView.as_view({'post': 'create'}), name='user-create'),
-#     # Other URLs...
-# ]
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('jwt/create/', CustomTokenObtainPairView.as_view()),
+    path('jwt/refresh/', CustomTokenRefreshView.as_view()),
+    path('jwt/verify/', CustomTokenVerifyView.as_view()),
+]
--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('auths.urls')),
-    path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.jwt')),
+    path('api/', include('auths.urls')),
     path('api/v1/', include('accounts.api.urls')),
 ]
