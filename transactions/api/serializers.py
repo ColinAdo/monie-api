6464e49e9d311c1dec9ad6a6e3d1162b7039c4ca
ColinAdo <colinadodev30@gmail.com>
2024-06-10 23:12:35 +0300

    Add comment to serializers file

--- transactions/api/serializers.py
+++ transactions/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from transactions.models import Transaction
 
+# Transaction serializer
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
