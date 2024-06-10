c688052f7c7fad70efb8b293527b1529c8c09403
ColinAdo <colinadodev30@gmail.com>
2024-06-10 23:10:43 +0300

    Add comment to admin file

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Transaction
 
+# Transaction admin
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ["account", "amount", "transaction_type", "created_date"]
 
