f31b6d30f07bfdc9890d164b2dd2005b04040a28
ColinAdo <colinadodev30@gmail.com>
2024-06-22 10:31:13 +0300

    Add removed phone number fields and using email instead

--- auths/models.py
+++ auths/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from phonenumber_field.modelfields import PhoneNumberField
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=200, unique=False)
-    phone_number = PhoneNumberField(unique=True)
-    pin = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255)
 
-    USERNAME_FIELD = 'phone_number'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
