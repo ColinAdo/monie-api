4091f60ab0f074832b15b00c3096b7c78dac9a33
ColinAdo <colinadodev30@gmail.com>
2024-06-22 10:32:21 +0300

    Add removed phone number Configs

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 
     # 3rd party libraries
     'rest_framework',
-    'phonenumber_field',
     'corsheaders',
 
     # local
@@ -171,8 +170,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-PHONENUMBER_DEFAULT_REGION = 'KE'
-PHONENUMBER_DEFAULT_FORMAT = 'E164'
 
 AUTH_USER_MODEL = 'auths.CustomUser'
