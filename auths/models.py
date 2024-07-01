8184f401e012d49d66848405f2d116b5123ce127
ColinAdo <colinadodev30@gmail.com>
2024-07-01 21:01:25 +0300

    Add commit to Custom user model

--- auths/models.py
+++ auths/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+# Custom user model
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=255)
