fd0ddc161bf46df310a6f24d95633532c69145cd
ColinAdo <colinadodev30@gmail.com>
2024-07-02 12:33:01 +0300

    Add single quots

--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/v1/', include('auths.urls')),
     path('api/v1/', include('accounts.api.urls')),
     path('api/v1/', include('transactions.api.urls')),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/avi/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/avi/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc',),
     path('api/v1/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
 ]
