9a6f52016a7d4e847fbf54137ab3e3b9d77f32b0
ColinAdo <colinadodev30@gmail.com>
2024-07-02 10:49:13 +0300

    Add test case to Custom JWTAuthenitcation class

--- auths/tests.py
+++ auths/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from rest_framework.test import APIRequestFactory
+from rest_framework_simplejwt.tokens import AccessToken
+
+from core.authentication import CustomJWTAuthentication
+
 # User test case
 class UserTestCase(TestCase):
 
@@ -17,3 +22,33 @@ class UserTestCase(TestCase):
         self.assertEqual(self.user.username, 'TestUser')
         self.assertEqual(self.user.email, 'test@example.com')
         self.assertEqual(str(self.user), 'TestUser')
+
+class CustomJWTAuthenticationTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+        cls.auth = CustomJWTAuthentication()
+
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpass'
+        )
+
+        cls.access_token = AccessToken.for_user(cls.user)
+
+    def test_authenticate_no_token(self):
+        request = self.factory.get('/')
+
+        request = self.auth.authenticate(request)
+
+        self.assertIsNone(request)
+
+    def test_authenticate_invalid_token(self):
+        request = self.factory.get('/')
+        request.META['HTTP_AUTHORIZATION'] = 'Bearer invalidtoken'
+        
+        result = self.auth.authenticate(request)
+        
+        self.assertIsNone(result)
