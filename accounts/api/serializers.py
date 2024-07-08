d649bbfd6d6cfc6e843545a0c60af0b39919e1c9
ColinAdo <colinadodev30@gmail.com>
2024-07-08 16:48:56 +0300

    Add comment to accounts serializers

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from accounts.models import Account
 from transactions.api.serializers import TransactionSerializer
 
+# Account serializers
 class AccountSerializer(serializers.ModelSerializer):
     transactions = TransactionSerializer(many=True, read_only=True)
     transactions_count = serializers.SerializerMethodField()
