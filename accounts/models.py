a77e55aa9a74ec9b6e132428ec2287c813e1ff92
ColinAdo <colinadodev30@gmail.com>
2024-07-08 16:48:03 +0300

    Add comment to accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 
+# Account model
 class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="accounts", on_delete=models.CASCADE)
     balance = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
