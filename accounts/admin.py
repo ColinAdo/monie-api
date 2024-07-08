040005cd70f66721023bdfe8c1d7c078b57bfbe5
ColinAdo <colinadodev30@gmail.com>
2024-07-08 16:47:43 +0300

    Add comment to accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from accounts.models import Account
 
+# Account admin
 class AccountAdmin(admin.ModelAdmin):
     list_display = ["user", "balance", "created_date"]
 
