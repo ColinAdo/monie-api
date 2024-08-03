d73da6181d7df0fb99e4c998c453b85266b6ef2a
ColinAdo <colinadodev30@gmail.com>
2024-08-03 09:36:30 +0300

    Add single quotes convention

--- transactions/api/urls.py
+++ transactions/api/urls.py
@@ -10,10 +10,10 @@ from transactions.api.views import (
 )
 
 router = SimpleRouter()
-router.register('transactions', TransactionViewSet, basename="transactions")
-router.register('withdraw', WithdrawTransaction, basename="withdraw")
-router.register('received', ReceivedTransaction, basename="received")
-router.register('airtime', AirtimeTransaction, basename="airtime")
-router.register('sent', SentTransaction, basename="sent")
+router.register('transactions', TransactionViewSet, basename='transactions')
+router.register('withdraw', WithdrawTransaction, basename='withdraw')
+router.register('received', ReceivedTransaction, basename='received')
+router.register('airtime', AirtimeTransaction, basename='airtime')
+router.register('sent', SentTransaction, basename='sent')
 
 urlpatterns = router.urls
