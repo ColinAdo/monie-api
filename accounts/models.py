3d19846e0683eed973df53878664f757c1022355
ColinAdo <colinadodev30@gmail.com>
2024-08-06 19:15:52 +0300

    Add single quotes

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Account model
 class Account(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="accounts", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='accounts', on_delete=models.CASCADE)
     balance = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
     created_date = models.DateTimeField(auto_now_add=True)
 
