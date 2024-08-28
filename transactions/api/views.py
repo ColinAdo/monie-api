2a058d2b0315ca4f7b53f9846508547d9dec2910
ColinAdo <colinadodev30@gmail.com>
2024-08-28 23:11:10 +0300

    Add proper code comment

--- transactions/api/views.py
+++ transactions/api/views.py
@@ -7,32 +7,32 @@ from transactions.api.serializers import (
     TransactionSerializer,
 )
 
-# Transaction view
+# Transaction viewset
 class TransactionViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
-# Airtime transaction view
+# Airtime transaction viewset
 class AirtimeTransaction(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.filter(transaction_type='airtime')
     serializer_class = TransactionSerializer
 
-# Sent transaction view
+# Sent transaction viewset
 class SentTransaction(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.filter(transaction_type='sent')
     serializer_class = TransactionSerializer
 
-# Received transaction view
+# Received transaction viewset
 class ReceivedTransaction(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.filter(transaction_type='received')
     serializer_class = TransactionSerializer
 
 
-# Withdrew transaction view
+# Withdrew transaction viewset
 class WithdrawTransaction(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.filter(transaction_type='withdraw')
