a9a8b40c59cdef0dbb35329216cf34213224ab4e
ColinAdo <colinadodev30@gmail.com>
2024-11-14 09:34:30 +0300

    Add account name and description

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,9 @@ from django.db import models
 # Account model
 class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='accounts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True, null=True)
     balance = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
+    description = models.CharField(max_length=255, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
