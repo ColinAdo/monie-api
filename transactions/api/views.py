3d862458e9eef976de2c3cc8993cdd1b51dde5ab
ColinAdo <colinadodev30@gmail.com>
2024-11-18 07:55:33 +0300

    Deleted transaction types views

--- transactions/api/views.py
+++ transactions/api/views.py
@@ -12,29 +12,3 @@ class TransactionViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-
-# Airtime transaction viewset
-class AirtimeTransaction(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-    queryset = Transaction.objects.filter(transaction_type='airtime')
-    serializer_class = TransactionSerializer
-
-# Sent transaction viewset
-class SentTransaction(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-    queryset = Transaction.objects.filter(transaction_type='sent')
-    serializer_class = TransactionSerializer
-
-# Received transaction viewset
-class ReceivedTransaction(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-    queryset = Transaction.objects.filter(transaction_type='received')
-    serializer_class = TransactionSerializer
-
-
-# Withdrew transaction viewset
-class WithdrawTransaction(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-    queryset = Transaction.objects.filter(transaction_type='withdraw')
-    serializer_class = TransactionSerializer
-
