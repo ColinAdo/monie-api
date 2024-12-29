290593df7e772d03843cd4234ff8b81a60832f52
ColinAdo <colinadodev30@gmail.com>
2024-12-29 21:50:14 +0300

    Deleted choices file

--- transactions/models.py
+++ transactions/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 from accounts.models import Account
-from .choices import TRANSACTION_TYPES
 
 # Transaction model
 class Transaction(models.Model):
     account = models.ForeignKey(Account, related_name='transactions', on_delete=models.CASCADE)
     amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
-    transaction_type = models.CharField(max_length=200, choices=TRANSACTION_TYPES)
+    transaction_type = models.CharField(max_length=200)
     description = models.CharField(max_length=200, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True, null=True)
 
