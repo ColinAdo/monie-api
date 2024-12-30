5b6a45db59bbf74f982b59b23c276f6a9d5c8fbc
ColinAdo <colinadodev30@gmail.com>
2024-12-30 11:07:04 +0300

    Add create transaction consumer

--- transactions/consumers.py
+++ transactions/consumers.py
@@ -1,9 +1,11 @@
 import json
 import logging
+from decimal import Decimal
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from accounts.models import Account
+from transactions.models import Transaction
 
 # Account consumer
 class AccountConsumer(AsyncWebsocketConsumer):
@@ -36,11 +38,11 @@ class AccountConsumer(AsyncWebsocketConsumer):
         print("Received", json.dumps(data, indent=2))
 
         operation = data['event']
-        account_name = data['data']['name']
+        account_name = data['data']['accountName']
         description = data['data']['description']
         amount = data['data']['amount']
 
-        # Send the message to the group
+        # Send the message to the group if create_account oppereation
         if operation == 'create_account':
             await self.channel_layer.group_send(
                 self.username,
@@ -52,6 +54,7 @@ class AccountConsumer(AsyncWebsocketConsumer):
                 }
             )
             await self.save_account(account_name, description, amount)
+        # Send the message to the group if update_account oppereation
         elif operation == 'update_account':
             account_id = data['data']['id']
             await self.channel_layer.group_send(
@@ -66,8 +69,24 @@ class AccountConsumer(AsyncWebsocketConsumer):
             )
             await self.save_updated_account(account_id, account_name, description, amount)
 
+        # Send the message to the group if create_transaction oppereation
+        elif operation == 'create_transaction':
+            transaction_type = data['data']['transactionType']
 
-    # Send the message to WebSocket
+            await self.channel_layer.group_send(
+                self.username,
+                {
+                    'type': 'create_transaction',
+                    'transaction_type': transaction_type,
+                    'description': description,
+                    'amount': amount,
+                    'account_name': account_name
+                }
+            )
+            await self.save_transaction(account_name, transaction_type, description, amount)
+
+
+    # Send the created account to WebSocket
     async def create_account(self, event):
         name = event['name']
         description = event['description']
@@ -79,6 +98,7 @@ class AccountConsumer(AsyncWebsocketConsumer):
             'amount': amount,
         }))
 
+     # Send the updated account to WebSocket
     async def update_account(self, event):
         name = event['name']
         description = event['description']
@@ -90,6 +110,19 @@ class AccountConsumer(AsyncWebsocketConsumer):
             'amount': amount,
         }))
 
+     # Send the created transaction to WebSocket
+    async def create_transaction(self, event):
+        account_name = event['account_name']
+        description = event['description']
+        transaction_type = event['transaction_type']
+        amount = event['amount']
+
+        await self.send(text_data=json.dumps({
+            'transaction_type': transaction_type,
+            'account_name': account_name,
+            'description': description,
+            'amount': amount,
+        }))
 
     @sync_to_async
     def save_account(self, account_name, description, amount):
@@ -107,3 +140,25 @@ class AccountConsumer(AsyncWebsocketConsumer):
             account.save()
         except Account.DoesNotExist:
             logging.error(f"Account with ID {account_id} does not exist or does not belong to the user.")
+
+    @sync_to_async
+    def save_transaction(self, account_name, transaction_type, description, amount):
+        
+        try:
+            account = Account.objects.get(name=account_name, user=self.scope.get('user'))
+            if transaction_type == 'income':
+                amount = Decimal(amount)
+                account.amount += amount 
+                account.save()
+            else:
+                amount = Decimal(amount)
+                account.amount -= amount 
+                account.save()
+            Transaction.objects.create(
+            account=account, 
+            transaction_type=transaction_type, 
+            description=description,
+            amount=amount
+        )
+        except Account.DoesNotExist:
+            logging.error(f"Account with ID {account} does not exist or does not belong to the user.")
