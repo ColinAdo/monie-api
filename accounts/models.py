5ddf7ee9662d48d2e6ab27d01ce69d2d100bbeea
ColinAdo <colinadodev30@gmail.com>
2024-12-31 16:07:04 +0300

    Add proper string length

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Account model
 class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='accounts', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=13, unique=True, blank=True, null=True)
     amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=18, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- transactions/models.py
+++ transactions/models.py
@@ -6,8 +6,8 @@ from accounts.models import Account
 class Transaction(models.Model):
     account = models.ForeignKey(Account, related_name='transactions', on_delete=models.CASCADE)
     amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
-    transaction_type = models.CharField(max_length=200)
-    description = models.CharField(max_length=200, null=True, blank=True)
+    transaction_type = models.CharField(max_length=7)
+    description = models.CharField(max_length=34, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
