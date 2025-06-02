ce72c4c86ef90694ec94c060cf624b2a667723a8
ColinAdo <colinadodev30@gmail.com>
2025-06-02 23:06:09 +0300

    Add reasonable max_decimal

--- transactions/models.py
+++ transactions/models.py
@@ -7,7 +7,7 @@ from auths.models import CustomUser
 class Transaction(models.Model):
     user = models.ForeignKey(CustomUser, related_name='transactions', on_delete=models.CASCADE, null=True, blank=True)
     account = models.ForeignKey(Account, related_name='transactions', on_delete=models.CASCADE)
-    amount = models.DecimalField(default=0.0, max_digits=99999999, decimal_places=2)
+    amount = models.DecimalField(default=0.0, max_digits=12, decimal_places=2)
     transaction_type = models.CharField(max_length=7)
     description = models.CharField(max_length=34, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True, null=True)
