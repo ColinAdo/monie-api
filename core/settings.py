08149f63e31de9d37c418f33a08f5628ee77b5d7
ColinAdo <colinadodev30@gmail.com>
2025-06-03 18:40:54 +0300

    add testing AUTH_COOKIE_SECURE

--- core/settings.py
+++ core/settings.py
@@ -170,8 +170,8 @@ SITE_NAME = 'Monie'
 # Cookies settings 
 AUTH_COOKIE = 'access'
 AUTH_COOKIE_MAX_AGE = 60 * 60 * 24
-AUTH_COOKIE_SECURE = True
-# AUTH_COOKIE_SECURE = os.getenv('AUTH_COOKIE_SECURE', 'True') == 'True'
+# AUTH_COOKIE_SECURE = True
+AUTH_COOKIE_SECURE = os.getenv('AUTH_COOKIE_SECURE', 'True') == 'True'
 AUTH_COOKIE_HTTP_ONLY = True
 AUTH_COOKIE_SAMESITE = 'None'
 AUTH_COOKIE_PATH = '/'
