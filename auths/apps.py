0e893b5f47830627e9c361110e6d8457a625c1a1
ColinAdo <colinadodev30@gmail.com>
2025-06-05 08:26:36 +0300

    Add create super user programatically

--- auths/apps.py
+++ auths/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
 
 
 class AuthsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auths'
+
+    def ready(self):
+        from django.db.utils import OperationalError
+        try:
+            User = get_user_model()
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+                    email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+                    password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+                )
+                print("✅ Superuser created.")
+        except OperationalError:
+            pass
