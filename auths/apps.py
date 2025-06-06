11d55f0d3729f67415d7a1fcf4a1c4fa0801fbf3
ColinAdo <colinadodev30@gmail.com>
2025-06-06 16:41:37 +0300

    Add proper code

--- auths/apps.py
+++ auths/apps.py
@@ -7,15 +7,15 @@ class AuthsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auths'
 
-    def ready(self):
-        from django.db.utils import OperationalError
-        try:
-            User = get_user_model()
-            if not User.objects.filter(is_superuser=True).exists():
-                User.objects.create_superuser(
-                    username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-                    email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-                    password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-                )
-        except OperationalError:
-            pass
+    # def ready(self):
+    #     from django.db.utils import OperationalError
+    #     try:
+    #         User = get_user_model()
+    #         if not User.objects.filter(is_superuser=True).exists():
+    #             User.objects.create_superuser(
+    #                 username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+    #                 email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+    #                 password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    #             )
+    #     except OperationalError:
+    #         pass
