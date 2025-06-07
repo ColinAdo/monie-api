0950dbf4eae4ce82a90aa4fa9de5ba3e6d433dd6
ColinAdo <colinadodev30@gmail.com>
2025-06-07 20:01:18 +0300

    Add associate_by_email to avoid two accounts with the same email address

--- core/settings.py
+++ core/settings.py
@@ -209,6 +209,20 @@ SOCIAL_AUTH_GITHUB_EXTRA_DATA = [
     ('name', 'name')
 ]
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+
+    'social_core.pipeline.social_auth.associate_by_email',
+
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 
 # Corsheaders settings
 CORS_ALLOWED_ORIGINS = os.getenv(
