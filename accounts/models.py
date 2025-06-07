d35d44415af6ccfdfd340fda7b9db9679c11c074
ColinAdo <colinadodev30@gmail.com>
2025-06-07 20:00:28 +0300

    Change account name from being unique

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Account model
 class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='accounts', on_delete=models.CASCADE)
-    name = models.CharField(max_length=13, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=13, blank=True, null=True)
     amount = models.DecimalField(default=0.0, max_digits=12, decimal_places=2)
     description = models.CharField(max_length=18, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
