6ac28db3af3cfc42bfd36a082a08b8a80cd42c86
ColinAdo <colinadodev30@gmail.com>
2025-06-09 11:48:36 +0300

    Add trial code

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+# Make sure migrations folder exists
+mkdir -p transactions/migrations/
 
-python manage.py migrate auths
-python manage.py migrate transactions
+# Force create initial migration if missing
+python manage.py makemigrations transactions --noinput || true
+
+# Apply migrations
 python manage.py migrate
-
