f49704909fe32033a3e27af7701bcf37b6d3e555
ColinAdo <colinadodev30@gmail.com>
2025-06-09 12:10:57 +0300

    Add code

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Only apply migrations (don't generate new ones)
+python manage.py makemigrations
+
 python manage.py migrate
