f023feacfb734712ec0e9f2c9ca3b6e9f7193b21
ColinAdo <colinadodev30@gmail.com>
2025-06-09 10:52:04 +0300

    Add build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+python manage.py makemigrations --noinput
 
-python manage.py migrate
+python manage.py migrate --noinput
