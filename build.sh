f3aba7326990f0ef48efb2c146215af90ce0c2db
ColinAdo <colinadodev30@gmail.com>
2025-06-09 12:08:41 +0300

    Add proper code

--- build.sh
+++ build.sh
@@ -4,15 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Create migrations for all apps
-mkdir -p auths/migrations/
-mkdir -p transactions/migrations/
-touch auths/migrations/__init__.py
-touch transactions/migrations/__init__.py
-
-# Generate migrations for all apps
-python manage.py makemigrations auths || echo "Could not make auths migrations"
-python manage.py makemigrations transactions || echo "Could not make transactions migrations"
-
-# Apply all migrations
+# Only apply migrations (don't generate new ones)
 python manage.py migrate
